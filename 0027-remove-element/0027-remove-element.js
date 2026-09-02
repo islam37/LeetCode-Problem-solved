29e2adf4ac72b233d5900f0d58cf4ac5a409f9ab
Islam Uddin <islamuddin3725@gmail.com>
2026-09-02 10:15:44 +0600

    Time: 0 ms (100%), Space: 53.1 MB (83.74%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.js
@@ -0,0 +1,12 @@
+var removeElement = function(nums, val) {
+    let k = 0; // Pointer for valid elements
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== val) {
+            nums[k] = nums[i];
+            k++;
+        }
+    }
+
+    return k;
+};
